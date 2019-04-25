c500ec745d01651aba0f38f83fd35019c495f88a
en <en@brucia.bibsys.no>
2019-04-25 11:53:05 +0200

    trappings

--- rumours/src/blargh/rpg/warhammer/RandomCharacterGenerator.java
+++ rumours/src/blargh/rpg/warhammer/RandomCharacterGenerator.java
@@ -1,12 +1,12 @@
 package blargh.rpg.warhammer;
 
 import static blargh.rpg.warhammer.Characteristics.NONE;
-import static blargh.rpg.warhammer.Races.HUMAN;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -42,10 +42,49 @@ public class RandomCharacterGenerator {
 	}
 	
 	public static void main(String... args) {
-		Random randomizer = new Random();
-		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get("/temp/soldier.txt")))
-		{
-			bufferedWriter.write(String.format(create("soldier", 3, HUMAN, randomizer)));
+//		Random randomizer = new Random();
+//		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get("/temp/soldier.txt")))
+//		{
+//			bufferedWriter.write(String.format(create("soldier", 3, HUMAN, randomizer)));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		try {
+			List<String> tempList = new ArrayList<>();
+			Files.lines(Paths.get("resources/trappings.txt")).forEach(line -> {
+//				System.out.println(line);
+				tempList.add(line);
+				if(tempList.size() == 8) {
+					String careerName = tempList.get(2).split(" - ")[0];
+					if("Giant Slayer".contentEquals(careerName)) {
+						careerName = "Slayer";
+					}
+					StringBuilder output = new StringBuilder("{%n")
+							.append("  career: \"").append(careerName).append("\",%n")
+							.append("  level: [%n");
+					for(int i=0;i<4;i++) {
+						if(i > 0) {
+							output.append(",%n");
+						}
+						String title = tempList.get(i*2);
+						String trappings = tempList.get(i*2+1).replace("Trappings: ", "");
+						String status = title.split(" - ")[1];
+						title = title.split(" - ")[0];
+						output.append(String.format("    {%n      name: \"%s\",%n      status: \"%s\",%n      trappings: \"%s\"%n    }", title, status, trappings));
+					}
+					output.append("%n  ]%n")
+					.append("}%n");
+					tempList.clear();
+					String fileName = careerName.replace(" ", "").toLowerCase() + "_trapping.json";
+					try(BufferedWriter bw = Files.newBufferedWriter(Paths.get("resources/careers/" + fileName))) {
+						bw.write(String.format(output.toString()));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					System.out.printf(output.toString());
+				}
+			});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
