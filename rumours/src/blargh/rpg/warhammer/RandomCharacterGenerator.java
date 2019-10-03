6cd41c578d4ef1443ef6b63b44da1093740ea669
en <eirik.nilsen@unit.no>
2019-10-03 13:02:15 +0200

    Added table to skills

--- rumours/src/blargh/rpg/warhammer/RandomCharacterGenerator.java
+++ rumours/src/blargh/rpg/warhammer/RandomCharacterGenerator.java
@@ -2,10 +2,6 @@ package blargh.rpg.warhammer;
 
 import static blargh.rpg.warhammer.Characteristics.NONE;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 import org.yogthos.JsonPDF;
@@ -26,6 +23,7 @@ public class RandomCharacterGenerator {
 
 	private static final String STAT_WIDTHS = "\"widths\": [2,1,1,1,1,1,1,1,1,1,1,1],";
 	private static final String CELL_ALIGN_CENTER = "[\"cell\", {\"align\": \"center\"}, ";
+	private static final String CELL_ALIGN_LEFT = "[\"cell\", {\"align\": \"left\"}, ";
 
 	public enum OutputType {
 		JSON,
@@ -86,7 +84,56 @@ public class RandomCharacterGenerator {
 	 
 	public static void writeToPdf(String fileName, String characterPresentation) {
 		
-		String jsonDoc = String.join("", "[{\"pages\": true, \"orientation\":\"landscape\"}", characterPresentation.replaceAll("----- Start -----", ", \\[\"paragraph\", \"").replaceAll("----- End -----", "\"\\]\\]"));
+		String jsonDoc = String.join("", "[{\"pages\": true, \"orientation\":\"landscape\"}", characterPresentation.replaceAll("----- Start -----", ", \\[\"paragraph\", \"")
+				.replaceAll("----- End -----", "\"\\]\\]"));
+//		jsonDoc = createStatPdf(characterPresentation, jsonDoc);
+		
+		AtomicBoolean collect = new AtomicBoolean(false);
+		List<String> skillList = new ArrayList<>();
+		Arrays.stream(characterPresentation.split("\\r?\\n")).forEach(line -> {
+			if(line.startsWith("Talents:")) {
+				collect.set(false);
+			}
+			if(collect.get() && !line.trim().isEmpty()) {
+				skillList.add(line);
+			}
+			if(line.startsWith("Skills:")){
+				collect.set(true);
+			}
+		});
+
+		jsonDoc = jsonDoc.replace("Skills:", "\"], [\"heading\", \"Skills:\"],[\"table\", {\"widths\": [2, 1, 1, 1]},");
+		jsonDoc = jsonDoc.replace("Talents:", "], [\"heading\", \"Talents:\"],[\"paragraph\", \"");
+		
+		List<String> skillPdfList = skillList.stream().map(line -> {
+			String newLine = line;
+			
+			while(newLine.indexOf("  ") > 0) {
+				newLine = newLine.replaceAll("  ", " ");
+			}
+			newLine = newLine.replace(" = ", " ");
+			newLine = newLine.replace("( ", ":(");
+			
+			String[] split = newLine.split(":");
+			String skillName = split[0].trim();
+			
+			newLine = String.join(",", Arrays.stream(split[1].split(" ")).map(value -> String.format("%s\"%s\"]", CELL_ALIGN_LEFT, value)).collect(Collectors.toList()));
+			newLine = String.join("", "[", String.format("%s\"%s\"], ", CELL_ALIGN_CENTER, skillName),  newLine, "]");
+			
+			return newLine;
+		}).collect(Collectors.toList());
+		skillPdfList.set(skillPdfList.size() - 1, String.join("", skillPdfList.get(skillPdfList.size() - 1), "]"));
+
+		for(int i=0;i<skillList.size();i++) {
+			jsonDoc = jsonDoc.replace(skillList.get(i), i+1 < skillList.size() ? String.format("%s,", skillPdfList.get(i)) : skillPdfList.get(i));
+		}
+		
+		System.out.println(jsonDoc);
+		
+		JsonPDF.writeToFile(jsonDoc, fileName, null);
+	}
+
+	private static String createStatPdf(String characterPresentation, String jsonDoc) {
 		jsonDoc = jsonDoc.replace("Stats:     M   WS   BS    S    T   AG    I  DEX  INT   WP  FEL", 
 				String.join("", "\"], [\"table\",{", 
 						STAT_WIDTHS,
@@ -103,17 +150,21 @@ public class RandomCharacterGenerator {
 						CELL_ALIGN_CENTER, "\"WP\"], ",
 						CELL_ALIGN_CENTER, "\"FEL\"]],",
 						"\"width\": 50}, ["));
-		List<String> statValueList = Arrays.stream(characterPresentation.split("\\r?\\n")).filter(line -> line.startsWith("Values:")).collect(Collectors.toList());
+		List<String> statValueList = Arrays.stream(characterPresentation.split("\\r?\\n"))
+				.filter(line -> line.startsWith("Values:"))
+				.collect(Collectors.toList());
 		for(String statLine: statValueList) {
 			String line = statLine.trim();
+			
 			while(line.indexOf("  ") > 0) {
 				line = line.replaceAll("  ", " ");
 			}
-			String join = "" + String.join(", ", Arrays.stream(line.split(" ")).map(value -> String.join("", "[\"cell\", {\"align\": \"center\", \"background-color\": [200, 200, 200]}, \"", value, "\"]")).collect(Collectors.toList())) + "]], [\"paragraph\", \"";
+			String join = "" + String.join(", ", Arrays.stream(line.split(" "))
+					.map(value -> String.join("", "[\"cell\", {\"align\": \"center\", \"background-color\": [200, 200, 200]}, \"", value, "\"]"))
+					.collect(Collectors.toList())) + "]], [\"paragraph\", \"";
 			jsonDoc = jsonDoc.replace(statLine, join);
 		}
-		
-		JsonPDF.writeToFile(jsonDoc, fileName, null);
+		return jsonDoc;
 	}
 	
 	public static void main(String... args) {
